775a2a8de8ecd916c4983890d73158137fcb7e65
David Vanegas <dvanegas@ccxc.us>
2022-11-10 11:08:05 -0500

    feat(.zshrc): Peco git functions.
    Add pull and checkout git commands with peco.

--- zsh/peco.zsh
+++ zsh/peco.zsh
@@ -15,7 +15,7 @@ function peco-vim() {
   [[ -n "$file" ]] && vim "${file[@]}"
 }
 
-peco-dir() {
+function peco-dir() {
   local dir
   dir=$(find ${1:-.} -maxdepth 4 -type d 2> /dev/null | peco) && cd "$dir" && 
   for precmd in $precmd_functions; do
@@ -24,18 +24,37 @@ peco-dir() {
   zle reset-prompt
 }
 
-prueba(){
+function kill-localhost(){
   local pid
   pid=$(lsof -i :3000 | peco | awk '{print $2}' | xargs kill -9)
-  # kill -9 <PID>
+}
+
+function peco-git-pull() {
+  local selected_branch=$(git branch --list | colrm 1 2 | peco)
+    if [ -n "$selected_branch" ]; then
+        BUFFER="git pull origin ${selected_branch}"
+        zle accept-line
+    fi
+}
+
+function peco-git-checkout () {
+    local selected_branch=$(git branch --list | colrm 1 2 | peco)
+    if [ -n "$selected_branch" ]; then
+        BUFFER="git switch ${selected_branch}"
+        zle accept-line
+    fi
 }
 
 zle -N peco-history
 zle -N peco-vim
 zle -N peco-dir 
-zle -N prueba 
+zle -N kill-localhost 
+zle -N peco-git-pull
+zle -N peco-git-checkout
 
 bindkey '^f' peco-dir
 bindkey '^r' peco-history
 bindkey '^n' peco-vim
-bindkey '^k' prueba 
+bindkey '^k' kill-localhost 
+bindkey '^p' peco-git-pull
+bindkey '^s' peco-git-checkout
