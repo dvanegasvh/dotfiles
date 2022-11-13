62e75cb7efaa00661f51b25e85abed6a92c890c2
David Vanegas <dvanegas@ccxc.us>
2022-11-12 21:08:07 -0500

    fix(nvim): Gitsigns symbol.
    Fix gitsingns symbol when created a new untracked file.

--- .config/nvim/after/plugin/gitsigns.rc.lua
+++ .config/nvim/after/plugin/gitsigns.rc.lua
@@ -1,4 +1,4 @@
-local status_ok, gitsigns = pcall(require, "gitsigns")
+local status_ok, gitsigns = pcall(require, "gitsigns")'
 if not status_ok then
   return
 end
@@ -10,6 +10,8 @@ gitsigns.setup {
     delete = { hl = "GitSignsDelete", text = "▎", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
     topdelete = { hl = "GitSignsDelete", text = "▎", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
     changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    untracked = { hl = 'GitSignsAdd'   , text = "▎", numhl='GitSignsAddNr'   , linehl='GitSignsAddLn' },
+
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
   numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
